78b57d6542aba6695a8305d4649c6fc574ce4f67
EikerR <57848645+pankercito@users.noreply.github.com>
2023-02-09 17:48:02 -0400

    Error de conexion en inicio de sesion solucionado Cambios en formularios/Se redujo el tamaño del navbar para mayor estetica/Nuevas Llamadas de base de datos con relaciones  - Se dividio en 2 la funcionalidad del area de _usuarios_ no hay errores hasta ahora si se encuentra uno avisar  - Se hicieron relaciones entre tablaspara mayor eficiencia y evitar redundancia de informacion  - Configuracion de _formulario.html_ (barbara) para coincidir con los datos de perfiles de la base de datos  - Se hicieron algunos ajustes en varios css para dar acomodar estilos  - Se edito un poco el logo "sadin" del index
    En caso de encontrar errores por favor reportar.

--- php/validation.php
+++ php/validation.php
@@ -2,38 +2,38 @@
 
 session_start();
 
-if(isset($_SESSION['nombredelusuario']))
-{
-	header('location:../principal.php');
+if(isset($_SESSION['nombredelusuario'])){
+
+    header('location:../principal.php');
 }
 
-if(isset($_POST['login']))
-{
+if(isset($_POST['login'])){
+
     include ("conect.php");
 
     $usuariolg = $_POST['userlg'];
     $pass = $_POST['passlg'];
 
-    $check = mysqli_query($connec,"SELECT * FROM registro r INNER JOIN perfiles p ON r.ci = p.ci WHERE user = '$usuariolg' AND pass = '$pass';");
+    $check = mysqli_query($connec,"SELECT * FROM registro r INNER JOIN perfiles p ON r.ci = p.ci WHERE user = '$usuariolg' AND pass = '$pass'");
     $nr = mysqli_num_rows($check);
 
 	
-if (!isset($_SESSION['nombredelusuario'])){
+    if (!isset($_SESSION['nombredelusuario'])){
 
-    if($nr == 1){
+        if($nr == 1){
         
-        $dataview= mysqli_fetch_assoc($check);
+            $dataview= mysqli_fetch_assoc($check);
 
-        $_SESSION['nombredelusuario']= $dataview['nombre'];
-        $_SESSION['apellidodelusuario']= $dataview['apellido'];
-        $_SESSION['admincheck']= $dataview['adp'];
+            $_SESSION['nombredelusuario']= $dataview['nombre'];
+            $_SESSION['apellidodelusuario']= $dataview['apellido'];
+            $_SESSION['admincheck']= $dataview['adp'];
 
-        // Redirecciono al usuario a la página principal del sitio.
-        header("HTTP/1.1 302 Moved Temporarily"); 
-        header('location:../principal.php?perfil=true');
-    }else {
-        header("HTTP/1.1 302 Moved Temporarily"); 
-	    header('location:../index.php?fallo=true');
+            // Redirecciono al usuario a la página principal del sitio.
+            header("HTTP/1.1 302 Moved Temporarily"); 
+            header('location:../principal.php?perfil=true');
+        }else {
+            header("HTTP/1.1 302 Moved Temporarily"); 
+	        header('location:../index.php?fallo=true');
         }
     }
 }
