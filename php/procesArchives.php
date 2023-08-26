9a9d90d3752095b8832487091e6b3e57f466ea08
EikerR <rodriguezeiker08@gmail.com>
2023-08-26 02:21:40 -0400

    Reestructuracion con clases sistema funcional #6 Reestructuracion con clases sistema funcional
    Falta
    - integracion final de personal
    - nomina,
    - dashboard principal
    bugs = no encontrados || reportar
    
    Co-authored-by: JesusJ4s <JesusJ4s@users.noreply.github.com>
    Co-authored-by: victorx2 <victorx2@users.noreply.github.com>

--- php/procesArchives.php
+++ php/procesArchives.php
@@ -7,11 +7,12 @@ include("../php/function/idGenerador.php");
 include("../php/function/sumarHora.php");
 include("../php/function/filesFunctions.php");
 
+$conn = new Conexion();
 // Escapar los caracteres especiales
-$taken = mysqli_real_escape_string($connec, $_POST["nameArchive"]);
-$tipeArch = mysqli_real_escape_string($connec, $_POST["gestionArch"]);
-$ci = desencriptar(mysqli_real_escape_string($connec, $_POST["ciArch"]));
-$note = mysqli_real_escape_string($connec, remover_acentos($_POST["textArchive"]));
+$taken = $conn->real_escape( $_POST["nameArchive"]);
+$tipeArch = $conn->real_escape($_POST["gestionArch"]);
+$ci = $conn->real_escape(desencriptar($_POST["ciArch"]));
+$note = $conn->real_escape(remover_acentos($_POST["textArchive"]));
 $carion = $_FILES["inpArch"]["name"];
 $arch = $_FILES["inpArch"]["tmp_name"];
 
@@ -21,7 +22,7 @@ $folDestino = '../data/archives/' . $tipeArch;
 // id para archivoss y Consulta de ID
 do {
     $id = generarId() . generarId();
-    $sql = mysqli_query($connec, "SELECT id_archivo FROM archidata WHERE id_archivo = $ci");
+    $sql = $conn->query("SELECT id_archivo FROM archidata WHERE id_archivo = $ci");
     $dan = mysqli_num_rows($sql);
 } while ($dan != 0);
 
@@ -44,7 +45,7 @@ if (moveFile($arch, $folDestino, $nombreArch) == true) {//mover archivos a la ru
 
     $sql1 = "INSERT INTO arch_direc (id_arch, id_user_sub, id_tipo, direccion, fecha) VALUES ('$id', '$idUserAg', '$tipeArch', '$direccion', '$fech')";
 
-    if (mysqli_query($connec, $sql) && mysqli_query($connec, $sql1)) {
+    if ($conn->query($sql) && $conn->query($sql1)) {
         echo "success";
 
     } else {
@@ -55,5 +56,3 @@ if (moveFile($arch, $folDestino, $nombreArch) == true) {//mover archivos a la ru
 } else {
     echo "error";
 }
-
-$connec->close();
